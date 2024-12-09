f414601c5a75203b1f05a6eb6e1a8bde2b3414e0
DaniRox <danirox.disenioydesarrollo@gmail.com>
2024-12-09 16:08:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch  } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import "./App.css";
+
+
+const defaultTodos = [
+    { text: "Cortar cebolla", completed: true },
+    { text: "Tomar el curso de ReactJs", completed: false },
+    { text: "Llorar con la llorona", completed: false },
+    { text: "Hacer las compras", completed: false },
+]
+
+function App() {
+    return (
+        <>
+            <TodoCounter completed={16} total={25} />
+            <TodoSearch />
+
+            <TodoList>
+            {defaultTodos.map(todo => (
+            <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed} />
+            ))}
+            </TodoList>
+
+            <CreateTodoButton />
+
+        </>
+    );
+}
+
+export default App;
